e906e177771dec6b7028cd053a53ac127c5eb837
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-09-07 12:23:29 -0600

    reactive forms para login y registro

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { MainComponent } from './pages/main/main.component';
@@ -14,8 +15,10 @@ import { MainComponent } from './pages/main/main.component';
     MainComponent
   ],
   imports: [
+    AuthRoutingModule,
     CommonModule,
-    AuthRoutingModule
+    ReactiveFormsModule
+    
   ]
 })
 export class AuthModule { }
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    name: [ 'Test 1', [Validators.required] ],
+    email: [ 'test1@test.com', [Validators.required, Validators.email] ],
+    password: [ '123456', [Validators.required, Validators.minLength(6)] ]
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  registro() {
+
   }
 
 }
